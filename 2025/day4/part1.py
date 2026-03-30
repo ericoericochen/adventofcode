52e320ad8469ee66f1b55014c22cf2024b981562
Eric Chen <deeplearningericchen@gmail.com>
2026-03-30 01:22:53 -0400

    2025 day 4

---
+++ 2025/day4/part1.py
@@ -0,0 +1,47 @@
+input_file = "./inputs/day4.txt"
+
+
+def read_grid(f: str):
+    grid = []
+    with open(f, "r") as f:
+        for line in f:
+            grid.append([c for c in line.strip()])
+    return grid
+
+
+def is_roll_at(grid: list[list[str]], i: int, j: int):
+    r, c = len(grid), len(grid[0])
+
+    if not 0 <= i < r or not 0 <= j < c:
+        return False
+
+    return grid[i][j] == "@"
+
+
+def get_accessible_rolls(grid: list[list[str]]) -> int:
+    rolls = 0
+    r, c = len(grid), len(grid[0])
+
+    for i in range(r):
+        for j in range(c):
+            if grid[i][j] != "@":
+                continue
+
+            coords = set(
+                [(i + dy, j + dx) for dy in range(-1, 2) for dx in range(-1, 2)]
+            ) - {(i, j)}
+
+            neighbor_rolls = sum(
+                [is_roll_at(grid, coord[0], coord[1]) for coord in coords]
+            )
+
+            if neighbor_rolls < 4:
+                rolls += 1
+
+    return rolls
+
+
+grid = read_grid(input_file)
+rolls = get_accessible_rolls(grid)
+
+print("rolls: ", rolls)
